0fde0c64ebd95148ca398af8759f24c7d2dbb7de
FreeIntelligence <free04381@gmail.com>
2022-02-15 14:30:28 -0300

    Se crea un menu simple para navegar por el sitio.

--- frontend/src/pages/invoices/Layout.jsx
+++ frontend/src/pages/invoices/Layout.jsx
@@ -1,10 +1,36 @@
-import { Outlet } from "react-router-dom";
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Layout() {
   return (
-    <>
-      <p>Desde layout</p>
-      <Outlet />
-    </>
+    <React.Fragment>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { md: 'flex' } }}>CENCOSUD</Typography>
+
+            <Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
+              <Link to="/invoices/create">
+                <Button sx={{ my: 2, color: 'white', display: 'block' }}>Crear factura</Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      
+      <Container maxWidth="sm">
+        <Outlet />
+      </Container>
+    </React.Fragment>
   );
 }
