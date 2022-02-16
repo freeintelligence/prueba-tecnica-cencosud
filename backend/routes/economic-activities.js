0d78759b48d2028e726699d30013bc039c872feb
FreeIntelligence <free04381@gmail.com>
2022-02-16 18:43:37 -0300

    Se crean los endpoints para crear giros y actividades economicas.

--- backend/routes/economic-activities.js
+++ backend/routes/economic-activities.js
@@ -1,10 +1,23 @@
 var express = require('express');
-const { EconomicActivity } = require('../models/economicActivity');
 var router = express.Router();
+const { EconomicActivity } = require('../models/economicActivity');
+const { body, validationResult } = require('express-validator');
 
 router.get('/', async function(req, res, next) {
   const documents = await EconomicActivity.find();
   res.json(documents);
+});
+
+router.post('/', body('name').isString(), body('name').isLength({ min: 1, max: 1024 }), async function(req, res) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const created = await EconomicActivity.create({ name: req.body.name });
+
+  return res.json(created);
 });
 
 module.exports = router;
--- backend/routes/economic-twists.js
+++ backend/routes/economic-twists.js
@@ -1,10 +1,23 @@
 var express = require('express');
-const { EconomicTwist } = require('../models/economicTwist');
 var router = express.Router();
+const { EconomicTwist } = require('../models/economicTwist');
+const { body, validationResult } = require('express-validator');
 
 router.get('/', async function(req, res, next) {
   const documents = await EconomicTwist.find();
   res.json(documents);
+});
+
+router.post('/', body('name').isString(), body('name').isLength({ min: 1, max: 1024 }), async function(req, res) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const created = await EconomicTwist.create({ name: req.body.name });
+
+  return res.json(created);
 });
 
 module.exports = router;
