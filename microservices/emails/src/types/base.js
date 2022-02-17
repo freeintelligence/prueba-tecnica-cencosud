7b45543d5da7e1dc2fc341289b7dab455d27a328
FreeIntelligence <free04381@gmail.com>
2022-02-17 11:07:43 -0300

    El sistema envia los correos de factura creada.

--- microservices/emails/src/types/base.js
+++ microservices/emails/src/types/base.js
@@ -1,15 +1,55 @@
+const { sendMail } = require('./../mail');
+const ejs = require('ejs');
+
 require('./../mail');
 
 module.exports = class BaseType {
 
-  data = {};
-
   constructor(data) {
     this.data = data;
+
+    this.viewHtml = null;
+    this.viewPlain = null;
+
+    this.defaultToName = null;
+    this.defaultToEmail = null;
+    this.defaultSubject = null;
   }
 
   async run() {
+    await this.sendInternalMail();
+  }
 
+  async getHtml(viewFile = null) {
+    return new Promise((resolve, reject) => {
+      ejs.renderFile(viewFile || this.viewHtml, this.data, {}, (err, html) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(html);
+      });
+    });
+  }
+
+  async getPlain(viewFile = null) {
+    return new Promise((resolve, reject) => {
+      ejs.renderFile(viewFile || this.viewPlain, this.data, {}, (err, plain) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(plain);
+      });
+    });
+  }
+
+  async sendInternalMail() {
+    await this.sendMail(this.defaultToName, this.defaultToEmail, this.defaultSubject, await this.getHtml(), await this.getPlain());
+  }
+
+  async sendMail(toName, toEmail, subject, text, html) {
+    return await sendMail(toName, toEmail, subject, text, html);
   }
 
 }
--- microservices/emails/src/types/invoice/index.js
+++ microservices/emails/src/types/invoice/index.js
@@ -1,13 +1,29 @@
+const path = require('path');
 const BaseType = require("../base");
 
 module.exports = class TypeInvoice extends BaseType {
 
   constructor(invoiceData) {
     super(invoiceData);
+
+    this.transmitterViewHtml = path.join(__dirname, 'transmitter-view.html.ejs');
+    this.transmitterViewPlain = path.join(__dirname, 'transmitter-view.plain.ejs');
+
+    this.receiverViewHtml = path.join(__dirname, 'receiver-view.html.ejs');
+    this.receiverViewPlain = path.join(__dirname, 'receiver-view.plain.ejs');
   }
 
   async run() {
-    console.log('llegó el email', this.data);
+    await this.sendTransmitterMail();
+    await this.sendReceiverMail();
+  }
+
+  async sendTransmitterMail() {
+    await this.sendMail(this.data.transmitterBusinessName, this.data.transmitterEmail, 'Factura emitida', await this.getPlain(this.transmitterViewPlain), await this.getHtml(this.transmitterViewHtml));
+  }
+
+  async sendReceiverMail() {
+    await this.sendMail(this.data.receiverContactName, this.data.receiverContactEmail, 'Factura emitida', await this.getPlain(this.receiverViewPlain), await this.getHtml(this.receiverViewHtml));
   }
 
 }
